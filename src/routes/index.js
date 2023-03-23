2728d741327cdadb5432b5052fc9afd13abe4730
jesusmiramontes-1180 <jesusm.miramontes@iteso.mx>
2023-03-23 16:40:19 -0600

    1.0.1

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,14 @@ const routesUsers = require('./users');
 const routesProducts = require('./products');
 const routesCards = require('./mastercards');
 const routesRatings = require('./ratings');
+const routesRestaurants = require('./restaurants');
+const routesOrders = require('./orders');
+const routesChats = require('./chats');
 
+router.use('/users', routesUsers);
+router.use('/restaurants', routesRestaurants);
+router.use('/orders', routesOrders);
+router.use('/chats', routesChats);
 router.use('/users', routesUsers);
 router.use('/products', routesProducts);
 router.use('/cards', routesCards);

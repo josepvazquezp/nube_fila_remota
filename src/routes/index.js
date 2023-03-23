ea2cf2bf30cc766cee4cfd9d4333f260d5cb6fd6
jesusmiramontes-1180 <jesusm.miramontes@iteso.mx>
2023-03-23 12:43:49 -0600

    1.0.0

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const routesUsers = require('./users');
+const routesProducts = require('./products');
+const routesCards = require('./mastercards');
+const routesRatings = require('./ratings');
 
 router.use('/users', routesUsers);
+router.use('/products', routesProducts);
+router.use('/cards', routesCards);
+router.use('/ratings', routesRatings);
 
 module.exports = router;
